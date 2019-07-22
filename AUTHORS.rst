60ab9c9ea9a38744f55ec149071678d9045d8ee7
drunkwretch <drunkwretch@gmail.com>
2019-07-22 19:56:55 +0800

    init repo

---
+++ AUTHORS.rst
@@ -0,0 +1,5 @@
+============
+Contributors
+============
+
+* drunkwretch <drunkwretch@gmail.com>
---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Dummy conftest.py for computing_intelligence.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+
+# import pytest
